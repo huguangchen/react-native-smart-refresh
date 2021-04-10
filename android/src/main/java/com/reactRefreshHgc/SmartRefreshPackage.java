aa1a7c79abfc37571c03147dc8791f14f95bf018
huguangchen <297144733@qq.com>
2021-04-10 18:00:28 +0800

    update

--- android/src/main/java/com/reactRefreshHgc/SmartRefreshPackage.java
+++ android/src/main/java/com/reactRefreshHgc/SmartRefreshPackage.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.reactRefreshHgc;
 
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -16,6 +16,11 @@ public class SmartRefreshPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(new SmartRefreshModule(reactContext));
     }
+
+//    @Override
+//    public List<Class<? extends JavaScriptModule>> createJSModules() {
+//        return null;
+//    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
